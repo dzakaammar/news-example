3979a840ad47dcf1e9b0360b58e53515c1d6c189
Dzaka Ammar Ibrahim <dzakaammar@gmail.com>
2018-07-08 13:43:19 +0700

    update service test

--- service/service_test.go
+++ service/service_test.go
@@ -26,9 +26,10 @@ func TestGetAllNewsByStatus(t *testing.T) {
 
 	excpectedResult := []*domain.News{news1}
 
-	actualResult, _ := service.GetAllNews(filterBy)
+	actualResult, err := service.GetAllNews(filterBy)
 
 	assert.Equal(t, excpectedResult, actualResult)
+	assert.Empty(t, err)
 }
 
 func TestGetAllNewsByTopics(t *testing.T) {
@@ -47,9 +48,11 @@ func TestGetAllNewsByTopics(t *testing.T) {
 
 	excpectedResult := []*domain.News{news1}
 
-	actualResult, _ := service.GetAllNews(filterBy)
+	actualResult, err := service.GetAllNews(filterBy)
 
 	assert.Equal(t, excpectedResult, actualResult)
+
+	assert.Empty(t, err)
 }
 
 func TestCreateNews(t *testing.T) {
@@ -59,17 +62,7 @@ func TestCreateNews(t *testing.T) {
 	news := &domain.News{
 		Title:   "Contoh",
 		Status:  "draft",
-		Content: "Contoh", /*
-			Topics: []*domain.Topic{
-				&domain.Topic{
-					ID:   1,
-					Name: "politik",
-				},
-				&domain.Topic{
-					ID:   1,
-					Name: "ekonomi",
-				},
-			}, */
+		Content: "Contoh",
 	}
 	newsRepository.On("Store", news, []string{"politik", "ekonomi"}).Return(nil)
 
@@ -77,5 +70,73 @@ func TestCreateNews(t *testing.T) {
 
 	err := service.CreateNews("Contoh", "draft", "Contoh", []string{"politik", "ekonomi"})
 
-	assert.Equal(t, nil, err)
+	assert.Empty(t, err)
+}
+
+func TestRemoveNews(t *testing.T) {
+	newsRepository := new(mocks.NewsRepository)
+	topicRepository := new(mocks.TopicRepository)
+
+	news := &domain.News{
+		ID:      1,
+		Title:   "Contoh",
+		Status:  "draft",
+		Content: "Contoh",
+	}
+
+	newsRepository.On("Find", uint(1)).Return(news, nil)
+	newsRepository.On("Remove", news).Return(nil)
+
+	service := NewService(newsRepository, topicRepository)
+
+	err := service.RemoveNews(1)
+	assert.Empty(t, err)
+}
+
+func TestGetAllTopics(t *testing.T) {
+	newsRepository := new(mocks.NewsRepository)
+	topicRepository := new(mocks.TopicRepository)
+
+	service := NewService(newsRepository, topicRepository)
+	topics := []*domain.Topic{
+		&domain.Topic{
+			ID:   1,
+			Name: "politik",
+		},
+		&domain.Topic{
+			ID:   2,
+			Name: "pendidikan",
+		},
+		&domain.Topic{
+			ID:   3,
+			Name: "budaya",
+		},
+	}
+	topicRepository.On("List").Return(topics, nil)
+
+	actual, err := service.GetAllTopic()
+
+	assert.Equal(t, topics, actual)
+
+	assert.Empty(t, err)
+
+}
+
+func TestRemoveTopic(t *testing.T) {
+	newsRepository := new(mocks.NewsRepository)
+	topicRepository := new(mocks.TopicRepository)
+
+	service := NewService(newsRepository, topicRepository)
+
+	topic := &domain.Topic{
+		ID:   1,
+		Name: "politik",
+	}
+
+	topicRepository.On("Find", uint(1)).Return(topic, nil)
+	topicRepository.On("Remove", topic).Return(nil)
+
+	err := service.RemoveTopic(1)
+
+	assert.Empty(t, err)
 }
